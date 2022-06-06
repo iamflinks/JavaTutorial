c54b613b381c0644281d8b92353125d66a3f1c39
FemiOlugbon <olugbonfemi@gmail.com>
2022-06-06 01:36:11 -0400

    Correct typo

---
+++ src/org/iamflinks/helloworld/Hello.java
@@ -0,0 +1,19 @@
+package org.iamflinks.helloworld;
+
+public class Hello {
+    public static void main(String[] args) {
+//        System.out.print("No new line \n");
+//        System.out.println("Hello, World!");
+        int num = -5;
+        System.out.println(num);
+
+        long numLong = 23344;
+        System.out.println(numLong);
+
+        double numDouble = 13.00099;
+        System.out.println(numDouble);
+
+        float numFloat = (float) 12.999099;
+        System.out.println(numFloat);
+    }
+}
