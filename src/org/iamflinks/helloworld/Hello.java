ea35ab1dcf53e7f15d67b79bb8acc0c82ecd20f4
FemiOlugbon <olugbonfemi@gmail.com>
2022-06-06 01:50:29 -0400

    Arithmetic in java

--- src/org/iamflinks/helloworld/Hello.java
+++ src/org/iamflinks/helloworld/Hello.java
@@ -4,27 +4,42 @@ public class Hello {
     public static void main(String[] args) {
 //        System.out.print("No new line \n");
 //        System.out.println("Hello, World!");
-        int num = -5;
-        System.out.println(num);
+//        int num = -5;
+//        System.out.println(num);
+//
+//        long numLong = 23344;
+//        System.out.println(numLong);
+//
+//        double numDouble = 13.00099;
+//        System.out.println(numDouble);
+//
+//        float numFloat = (float) 12.999909;
+//        System.out.println(numFloat);
+//
+//        char myChar = '\u00A9';
+//        System.out.println(myChar);
+//
+//        String name = "iamflinks";
+//        System.out.println(name.toUpperCase());
+//
+//        boolean myBoolean = true;
+//        if (myBoolean){
+//            System.out.println("Boolean True");
+        int a = 6;
+        int b = 47;
+        // Arithmetic
+        int add = a + b;
+        System.out.println(add);
 
-        long numLong = 23344;
-        System.out.println(numLong);
+        int sub = a - b;
+        System.out.println(sub);
 
-        double numDouble = 13.00099;
-        System.out.println(numDouble);
+        int mul = a * b;
+        System.out.println(mul);
 
-        float numFloat = (float) 12.999909;
-        System.out.println(numFloat);
+        double div = a / b;
+        System.out.println(div);
 
-        char myChar = '\u00A9';
-        System.out.println(myChar);
 
-        String name = "iamflinks";
-        System.out.println(name.toUpperCase());
-
-        boolean myBoolean = true;
-        if (myBoolean){
-            System.out.println("Boolean True");
-        }
     }
 }
